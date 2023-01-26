be3273d9541eff92cb1cd539c387283257973bf2
Sonwabiso <fsonwa@gmail.com>
2023-01-26 05:27:41 -0800

    updated

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * _printf: prints out a function
+ * Return: Always 0
+ **/
+
+int main(void)
+
+{
+
+	    _printf("%b\n", 98);
+
+	        return (0);
+
+}
